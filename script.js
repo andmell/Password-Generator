c6d02205a92120e24169d125990a612e750b5433
ahmell <andrewmell10@gmail.com>
2023-07-02 18:56:10 -0500

    comments

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
+  function generatePassword(){
+
+}
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+/* Clicking on Generate password should generate a series of prompts.
+the following prompts should ask the user:
+- how long the password is (between 8 and 128 characters) [input prompt]
+- whether it includes uppercase, lowercase, numeric, and/or special characters [boolean] []
+- generate password based on above metrics
+*/
+
+
