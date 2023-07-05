fe031776774415314f48d637325cedf339948d31
ahmell <andrewmell10@gmail.com>
2023-07-05 17:35:50 -0500

    deleted all redundant commented-out code, and updated README

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Password-Generator
-A tool designed to generate a secure password. Javascript-heavy
+
+## Description
+The purpose of this project was to develop a way to produce a password, producing different and random results depending on the criteria chosen by the user. The user is able to choose
+if their password will have upper or lowercase characters, numbers, and/or special characters. Through Javascript, a password will be generated based on the user's selection criteria.
+
+## Installation
+As this is a web application, no installation is necessary.
+
+## Usage
+Navigate to the site page [here]() and click "generate password". The site will prompt you with several options as to what characters you would like your password to contain, as well as the password length. After all prompts have been answered correctly, your newly generated password will appear in the text box, located center of the screen. 
 
 ## Credits
 - [A way to ensure a prompt only accepts numeric data.](https://stackoverflow.com/questions/46552085/javascript-force-specific-data-type-input-or-accept-only-digits-in-input) by Umang Galaiya on Stack Overflow.
 - [How to get a random item from an array.](https://www.programiz.com/javascript/examples/get-random-item) Answer found on programiz.
 - [A method on how to create a random password from a charset.](https://stackoverflow.com/questions/1497481/javascript-password-generator) Answered by Gumbo on stackoverflow, edited by Skizo.
 - [How to check for multiple booleans inside an if statement.](https://stackoverflow.com/questions/42358011/if-statement-checking-for-multiple-boolean-conditions) Answered by NIDHIN VINCENT on stackoverflow. Edited by fredmaggiowski.
+- A tutoring session with Jacob Carver, found through tutoring sessions offered through Washington University's Coding Bootcamp program. Code refactoring and returning the output to the user was discussed.
+
+## License
+Please refer to the github repository for licensing info.
--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
-// program to get a random item from an array
-
-// function getRandomItem (arr) {
-
-//   // get random index value
-//   const randomIndex = Math.floor(Math.random() * arr.length);
-//   // get random
-//   const item = arr[randomIndex];
-
-//   return item;
-// }
-
-// const arrayNumeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-// const arrayAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-// const arraySpecial = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '~', '`'];
-
-
-
-
-// const result = getRandomItem(arrayNumeric + arrayAlpha + arraySpecial);
-// console.log(result);
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -31,21 +7,21 @@ function writePassword() {
 
   function generatePassword(){
     let passwordInteger = prompt("Specify a length for your password","A number between 8 and 128");
-    while (!/^[0-9]+$/.test(passwordInteger)) { //a super complex boolean meant to test if the input is actually a number and not some nonsense. Thank you Umang Galaiya for this!
+    while (!/^[0-9]+$/.test(passwordInteger)) { //a line of regex meant to test if the input is actually a number and not some nonsense. Thank you Umang Galaiya for this!
     alert("You did not enter a number."); return
     }if (passwordInteger < 8) {
       alert("Please choose a number between 8 and 128"); return //need to return after the alert to ensure that the function ceases if a wrong number is used.
     } else if (passwordInteger > 128) {
       alert("Please choose a number between 8 and 128"); return
     } 
-    let passwordUppercase = confirm("Should your password have uppercase letters?");
+    let passwordUppercase = confirm("Should your password have uppercase letters?"); //This confirm message, and the following three, are necessary booleans to determine the character base of the generated password.
     let passwordLowercase = confirm("Should your password have lowercase letters?");
     let passwordNumeric = confirm("Should your password have numbers in it?");
     let passwordSpecial = confirm("Should your password have special characters in it?");
-    let characterBase = ""
-    let password = ""
+    let characterBase = "" // this will be the pool of characters that the for-loop will pull from to go into the generated password.
+    let password = "" // the final output of the password.
 
-    if (passwordUppercase){
+    if (passwordUppercase){ //totally did not know that if-statements could be standalone. Duh.
       characterBase += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     } 
     if (passwordLowercase){
@@ -58,140 +34,11 @@ function writePassword() {
       characterBase += "~!@#$%^&*()_+-="
     }  
     for (i = 0; i < passwordInteger; i++) {
-          password += characterBase.charAt(Math.floor(Math.random() * (characterBase.length)));
+          password += characterBase.charAt(Math.floor(Math.random() * (characterBase.length))); //the issue with using a math.random method is that, potentially some characters might be missed if a smaller password integer is used. 
         }
       return password;
-    // if (passwordUppercase === true && passwordLowercase === false && passwordNumeric === false && passwordSpecial === false) {
-    //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    //   passWrite = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passWrite += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passWrite);
-    //     password = passWrite;
-    //   }
-    // } else if (passwordLowercase === true && passwordNumeric === false && passwordSpecial === false && passwordUppercase === false) {
-    //   charset = "abcdefghijklmnopqrstuvwxyz"
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordLowercase === false && passwordNumeric === true && passwordSpecial === false && passwordUppercase === false) {
-    //   charset = "0123456789"
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordLowercase === false && passwordNumeric === false && passwordSpecial === true && passwordUppercase === false) {
-    //   charset = "~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordUppercase === true && passwordLowercase === true && passwordNumeric === false && passwordSpecial === false){
-    //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordUppercase === true && passwordLowercase === true && passwordNumeric === true && passwordSpecial === false){
-    //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    //   return passwordText;
-    // } else if (passwordUppercase === true && passwordLowercase === true && passwordNumeric === true && passwordSpecial === true) {
-    //   charset = "BCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordUppercase === true && passwordNumeric === true && passwordLowercase === false && passwordSpecial === false){
-    //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordUppercase === true && passwordNumeric === true && passwordSpecial === true && passwordLowercase === false){
-    //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordUppercase === true && passwordSpecial === true && passwordNumeric === false && passwordLowercase === false){
-    //   charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordLowercase === true && passwordNumeric === true && passwordSpecial === false && passwordUppercase === false){
-    //   charset = "abcdefghijklmnopqrstuvwxyz0123456789"
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordLowercase === true && passwordNumeric === true && passwordSpecial === true && passwordUppercase === false){
-    //   charset = "abcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordLowercase === true && passwordSpecial === true && passwordUppercase === false && passwordNumeric === false){
-    //   charset = "abcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } else if (passwordNumeric === true && passwordSpecial === true && passwordUppercase === false && passwordLowercase === false){
-    //   charset = "0123456789~!@#$%^&*()_+-="
-    //   passwordText = "";
-    //   for (i = 0; i < passwordInteger; i++) {
-    //     passwordText += charset.charAt(Math.floor(Math.random() * (charset.length)));
-    //     console.log(passwordText);
-    //   }
-    // } 
   }
-  
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-/* Clicking on Generate password should generate a series of prompts.
-the following prompts should ask the user:
-- how long the password is (between 8 and 128 characters) [input prompt]
-- whether it includes uppercase, lowercase, numeric, and/or special characters [boolean] []
-- generate password based on above metrics
-
-Possible combinations:
-Just uppercase letters ----
-Just lowercase letters ----
-Just numbers ----
-Just special characters ---- 
---
-uppercase + lowercase ----
-uppercase + lowercase + numbers ----
-uppercase + lowercase + numbers + special characters ----
-uppercase + numbers ----
-uppercase + numbers + special characters ----
-uppercase + special characters ----
-lowercase + numbers ----
-lowercase + numbers + special characters ----
-lowercase + special characters ----
-numbers + special characters
-*/
-
 
